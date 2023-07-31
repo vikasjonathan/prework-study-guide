1fa6a1919162c912bbde33fd51dddd8cc9517575
Vikas David Jonathan <vikas.jonathan@gmail.com>
2023-07-31 13:18:19 -0500

    added java script to the page with variables and for loop

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var topics=['HTML', 'CSS', 'Git', 'JavaScript']; 
+for (var x=0;x<topics.length;x++)
+    {console.log (topics[x]);}
+if(topics==='HTML') {
+    console.log("Let's study HTML!");
+} else if (topics==='CSSS') {
+    console.log("Let's study CSS");
+} else if (topics=== 'GIT') {
+    console.log ("Let's Study Git!");
+} else if (topics==='JavaScript'){
+    console.log("Let's Study JavaScript!");
+} else {
+    console.log('Please try again');
+}
